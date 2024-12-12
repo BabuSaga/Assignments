647e73588bea47293b04be39815d22039755a8d0
Balachander Rao Palepu <palepubalachanderrao25@gmail.com>
2024-12-11 20:01:08 -0500

    numbers to word 0-100

---
+++ numberToWord.go
@@ -0,0 +1,119 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("Enter a number (0-100): ")
+	fmt.Scan(&n) 
+	{
+		fmt.Print("Number in words: ")
+	if n == 0 {
+		fmt.Print("zero")
+	} else if n == 1 {
+		fmt.Print("one")
+	} else if n == 2 {
+		fmt.Print("two")
+	} else if n == 3 {
+		fmt.Print("three")
+	} else if n == 4 {
+		fmt.Print("four")
+	} else if n == 5 {
+		fmt.Print("five")
+	} else if n == 6 {
+		fmt.Print("six")
+	} else if n == 7 {
+		fmt.Print("seven")
+	} else if n == 8 {
+		fmt.Print("eight")
+	} else if n == 9 {
+		fmt.Print("nine")
+	} else if n == 10 {
+		fmt.Print("ten")
+	} else if n == 11 {
+		fmt.Print("eleven")
+	} else if n == 12 {
+		fmt.Print("twelve")
+	} else if n == 13 {
+		fmt.Print("thirteen")
+	} else if n == 14 {
+		fmt.Print("fourteen")
+	} else if n == 15 {
+		fmt.Print("fifteen")
+	} else if n == 16 {
+		fmt.Print("sixteen")
+	} else if n == 17 {
+		fmt.Print("seventeen")
+	} else if n == 18 {
+		fmt.Print("eighteen")
+	} else if n == 19 {
+		fmt.Print("nineteen")
+	} else if n == 20 {
+		fmt.Print("twenty")
+	} else if n > 20 && n < 30 {
+		fmt.Print("twenty ")
+		n = n - 20
+	} else if n == 30 {
+		fmt.Print("thirty")
+	} else if n > 30 && n < 40 {
+		fmt.Print("thirty ")
+		n = n - 30
+	} else if n == 40 {
+		fmt.Print("forty")
+	} else if n > 40 && n < 50 {
+		fmt.Print("forty ")
+		n = n - 40
+	} else if n == 50 {
+		fmt.Print("fifty")
+	} else if n > 50 && n < 60 {
+		fmt.Print("fifty ")
+		n = n - 50
+	} else if n == 60 {
+		fmt.Print("sixty")
+	} else if n > 60 && n < 70 {
+		fmt.Print("sixty ")
+		n = n - 60
+	} else if n == 70 {
+		fmt.Print("seventy")
+	} else if n > 70 && n < 80 {
+		fmt.Print("seventy ")
+		n = n - 70
+	} else if n == 80 {
+		fmt.Print("eighty")
+	} else if n > 80 && n < 90 {
+		fmt.Print("eighty ")
+		n = n - 80
+	} else if n == 90 {
+		fmt.Print("ninety")
+	} else if n > 90 && n < 100 {
+		fmt.Print("ninety ")
+		n = n - 90
+	} else if n == 100 {
+		fmt.Print("one hundred")
+	}
+
+	if n > 0 && n < 10 {
+	switch n {
+		 case 1:
+			fmt.Print("one")
+		 case 2:
+			fmt.Print("two")
+		 case 3:
+			fmt.Print("three")
+		 case 4:
+			fmt.Print("four")
+		 case 5:
+			fmt.Print("five")
+		 case 6:
+			fmt.Print("six")
+		 case 7:
+			fmt.Print("seven")
+		 case 8:
+			fmt.Print("eight")
+		 case 9:
+			fmt.Print("nine")
+	}
+	}
+	fmt.Println()
+	}
+	}
